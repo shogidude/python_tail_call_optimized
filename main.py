2f4beee391b7a32944795ee2fc55a2bd9f1feda2
Backrooms.net <94855537+shogidude@users.noreply.github.com>
2023-11-28 09:40:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-import sys
-
-
-class TailCallException(Exception):
-    def __init__(self, args, kwargs):
-        self.args = args
-        self.kwargs = kwargs
-
-
-def tail_call_optimized(func):
-    def wrapper(*args, **kwargs):
-        f = sys._getframe()
-        if f.f_back and f.f_back.f_back and f.f_back.f_back.f_code == f.f_code:
-            raise TailCallException(args, kwargs)
-        else:
-            while True:
-                try:
-                    return func(*args, **kwargs)
-                except TailCallException as e:
-                    args = e.args
-                    kwargs = e.kwargs
-
-    return wrapper
+from tailcallexception import *
 
 
 @tail_call_optimized
